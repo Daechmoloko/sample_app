755a6e93d4befb4c947dc44fbbe059f7462fa0c5
Asd <allundercontroll@yandex.ru>
2018-06-05 21:23:26 +0300

    Finish 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
